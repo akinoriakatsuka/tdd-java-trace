bd0cd1f19b15570935dcb1cd819f5c04193a1aa1
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-03-21 18:46:46 +0900

    [update] 不要なテストを削除
    timesの処理はcurrencyが"USD"であっても"CHF"であっても同じなので片方あれば良い

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -19,11 +19,4 @@ public class MoneyTest {
         assertFalse(Money.dollar(5).equals(Money.franc(5)));
     }
 
-    @Test
-    public void testFrancMultiplication() {
-        Money five = Money.franc(5);
-        assertEquals(Money.franc(10), five.times(2));
-        assertEquals(Money.franc(15), five.times(3));
-    }
-
 }
