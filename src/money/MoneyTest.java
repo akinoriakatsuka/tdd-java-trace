b5619c7dec6ae1d4c0a8a8ddd2a8cb4764a9a680
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-03-26 23:30:19 +0900

    [add] 足し算のテストを作成
    コンパイルエラーを確認

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -19,4 +19,9 @@ public class MoneyTest {
         assertFalse(Money.dollar(5).equals(Money.franc(5)));
     }
 
+    @Test
+    public void testSimpleAddition() {
+        Money sum = Money.dollar(5).plus(Money.dollar(5));
+        assertEquals(Money.dollar(10), sum);
+    }
 }
