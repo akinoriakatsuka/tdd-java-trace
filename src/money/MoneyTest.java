785086f56e2408750cc595c561cb439e69a9db58
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-03-27 23:12:57 +0900

    [update] 通貨の交換倍率をMapで管理するように変更

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -49,7 +49,21 @@ public class MoneyTest {
     public void testReduceMoneyDifferentCurrency() {
         Bank bank = new Bank();
         bank.addRate("CHF", "USD", 2);
-        Money result = bank.reduce(Money.franc(2),"USD");
-        assertEquals(Money.dollar(1),result);
+        Money result = bank.reduce(Money.franc(2), "USD");
+        assertEquals(Money.dollar(1), result);
+    }
+
+    @Test
+    public void testGetRate() {
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        assertEquals(2, bank.rate("CHF", "USD"));
+        assertEquals(1, bank.rate("USD", "USD"));
+    }
+
+    @Test
+    public void testPairEquality() {
+        assertEquals(new Pair("USD", "CHF"), new Pair("USD", "CHF"));
+        assertNotEquals(new Pair("USD", "CHF"), new Pair("USD", "USD"));
     }
 }
