691ac1ac65246bb3602400b05e923ab18cfac746
akinoriakatsuka <akinoriakatsuka@gmail.com>
2023-03-28 13:27:26 +0900

    [update] Sumにplusメソッドを実装

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -72,8 +72,19 @@ public class MoneyTest {
         Expression fiveBucks = Money.dollar(5);
         Expression tenFrancs = Money.franc(10);
         Bank bank = new Bank();
-        bank.addRate("CHF","USD",2);
-        Money result = bank.reduce(fiveBucks.plus(tenFrancs),"USD");
-        assertEquals(Money.dollar(10),result);
+        bank.addRate("CHF", "USD", 2);
+        Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+        assertEquals(Money.dollar(10), result);
+    }
+
+    @Test
+    public void testSumPlusMoney() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFranks = Money.franc(12);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+        Expression sum = new Sum(fiveBucks, tenFranks).plus(fiveBucks);
+        Money result = bank.reduce(sum, "USD");
+        assertEquals(result, Money.dollar(16));
     }
 }
